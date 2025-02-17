627de141ae0fe7775868431e7f1e7b59698e1182
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-17 22:21:40 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-600 via-pink-500 to-blue-500 ">
+    <h1
+      className={` text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-200 p-4`}
+    >
+      Hi I&apos;m Kalyan
+    </h1>
+  </div>
+
+  )
+}
+
+export default App
